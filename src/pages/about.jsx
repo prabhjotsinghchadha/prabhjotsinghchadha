628d40abc1daf7fcf63a356e16902f28ca8a2385
Prabhjot <prabhjottechs@gmail.com>
2023-08-06 21:05:49 +0530

    Updating Latest Information

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,120 +4,148 @@ import Link from 'next/link'
 import clsx from 'clsx'
 
 import { Container } from '@/components/Container'
-import {
-  TwitterIcon,
-  InstagramIcon,
-  GitHubIcon,
-  LinkedInIcon,
-} from '@/components/SocialIcons'
+import { TwitterIcon, InstagramIcon, GitHubIcon, LinkedInIcon } from '@/components/SocialIcons'
 import portraitImage from '@/images/portrait.jpg'
 
 function SocialLink({ className, href, children, icon: Icon }) {
-  return (
-    <li className={clsx(className, 'flex')}>
-      <Link
-        href={href}
-        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
-      >
-        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
-        <span className="ml-4">{children}</span>
-      </Link>
-    </li>
-  )
+    return (
+        <li className={clsx(className, 'flex')}>
+            <Link
+                href={href}
+                className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+            >
+                <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+                <span className="ml-4">{children}</span>
+            </Link>
+        </li>
+    )
 }
 
 function MailIcon(props) {
-  return (
-    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
-      <path
-        fillRule="evenodd"
-        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
-      />
-    </svg>
-  )
+    return (
+        <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+            <path
+                fillRule="evenodd"
+                d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
+            />
+        </svg>
+    )
 }
 
 export default function About() {
-  return (
-    <>
-      <Head>
-        <title>About - Prabhjot Singh Chadha</title>
-        <meta
-          name="description"
-          content="I’m Prabhjot. I live in India, where I design the future."
-        />
-      </Head>
-      <Container className="mt-16 sm:mt-32">
-        <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
-          <div className="lg:pl-20">
-            <div className="max-w-xs px-2.5 lg:max-w-none">
-              <Image
-                src={portraitImage}
-                alt=""
-                sizes="(min-width: 1024px) 32rem, 20rem"
-                className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
-              />
-            </div>
-          </div>
-          <div className="lg:order-first lg:row-span-2">
-            <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’m Prabhjot. I live in India, where I design the
-              future.
-            </h1>
-            <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
-              <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 7 years old, When I was in
-                school. Computer lab was my fav place to hangout. I use to play
-                lots of games in it and always wondered how these games are made
-                in background.</p>
-              <p>
-                During my first year of Bachlor of Engineering I took coding as
-                serious hobby. I created few blogging website on Wordpress, and
-                got popular with it. And i started creating iOS app during college
-                when it was Swift 2. It was fun.
-              </p>
-              <p>
-                After graduation I joined L&T Technology Services as full time software
-                enginner. And started my professional development journey from there. I was
-                main developer who use to handel all Internal DB and Internal apps. Got awarded
-                multiple times during Covid period. My dedication towards my projects gave
-                lot of great experience. But in 2021 I left the company because of Low pay scale
-                and I have cluster headache problem since college.
-              </p>
-              <p>
-                Today, I work as freelancer on Upwork, I have few clients who gives me
-                work on regular basis. But I am looking for some serious project to work
-                on. After leaving my previous company I have skilled up really great. and
-                hope i will find new role asap.
-              </p>
-            </div>
-          </div>
-          <div className="lg:pl-20">
-            <ul role="list">
-              <SocialLink href="https://twitter.com/tweet_prabhjot" icon={TwitterIcon}>
-                Follow on Twitter
-              </SocialLink>
-              <SocialLink href="https://instagram.com/insta_prabhjot" icon={InstagramIcon} className="mt-4">
-                Follow on Instagram
-              </SocialLink>
-              <SocialLink href="https://github.com/prabhjotsinghchadha" icon={GitHubIcon} className="mt-4">
-                Follow on GitHub
-              </SocialLink>
-              <SocialLink href="https://linkedin.com/in/mrprabhjot/" icon={LinkedInIcon} className="mt-4">
-                Follow on LinkedIn
-              </SocialLink>
-              <SocialLink
-                href="mailto:prabhjottechs@gmail.com"
-                icon={MailIcon}
-                className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
-              >
-                prabhjottechs@gmail.com
-              </SocialLink>
-            </ul>
-          </div>
-        </div>
-      </Container>
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>About - Prabhjot Singh Chadha</title>
+                <meta
+                    name="description"
+                    content="I’m Prabhjot. I live in India, where I design the future."
+                />
+            </Head>
+            <Container className="mt-16 sm:mt-32">
+                <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+                    <div className="lg:pl-20">
+                        <div className="max-w-xs px-2.5 lg:max-w-none">
+                            <Image
+                                src={portraitImage}
+                                alt=""
+                                sizes="(min-width: 1024px) 32rem, 20rem"
+                                className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+                            />
+                        </div>
+                    </div>
+                    <div className="lg:order-first lg:row-span-2">
+                        <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+                            I’m Prabhjot. I live in India, where I design the future.
+                        </h1>
+                        <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+                            <p>
+                                Hello and welcome! As passionate Software Engineer and Technical
+                                Lead with over six years of experience. I&apos;ve been immersed in
+                                the vibrant world of technology from an early age, intrigued by the
+                                underlying mechanics of computer games and software.
+                            </p>
+                            <p>
+                                From writing my first program at 7, to designing blogging websites
+                                on WordPress and dabbling with iOS apps during the Swift 2 era, I
+                                was honing my coding skills even before my formal education in
+                                engineering.
+                            </p>
+                            <p>
+                                After graduation, I kickstarted my professional journey at L&T
+                                Technology Services where I managed crucial internal databases and
+                                applications. My dedicated efforts were acknowledged with multiple
+                                awards during the Covid period. However, issues related to
+                                compensation and health prompted me to seek new horizons, leading me
+                                to the freelance world on Upwork.
+                            </p>
+
+                            <p>
+                                Today, I am a Technical Lead at FPS Lounge, where I use my technical
+                                prowess to shape the future of gaming. At FPS, I am building a
+                                transformative coaching platform for gamers, enabling them to hire
+                                top professional gamers from around the globe. As a key
+                                decision-maker, I play a pivotal role in the evolution of our
+                                product, solving complex problems, leading a team of talented
+                                developers, and continually refining our platform to enhance user
+                                experience.{' '}
+                            </p>
+                            <p>
+                                My journey through diverse projects and roles has armed me with an
+                                ability to navigate complex challenges, especially in the realm of
+                                web-based applications. My past experiences have endowed me with the
+                                ability to make impactful decisions, develop effective solutions,
+                                and lead teams with passion and purpose. I am now eager to take
+                                these skills to new heights, bringing value and innovation wherever
+                                I go.
+                            </p>
+                            <p>
+                                Thank you for visiting my page. I am always excited to connect with
+                                like-minded individuals and potential collaborators. Let&apos;s
+                                create something remarkable together!{' '}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="lg:pl-20">
+                        <ul role="list">
+                            <SocialLink
+                                href="https://twitter.com/tweet_prabhjot"
+                                icon={TwitterIcon}
+                            >
+                                Follow on Twitter
+                            </SocialLink>
+                            <SocialLink
+                                href="https://instagram.com/prabhjot.singh.chadha"
+                                icon={InstagramIcon}
+                                className="mt-4"
+                            >
+                                Follow on Instagram
+                            </SocialLink>
+                            <SocialLink
+                                href="https://github.com/prabhjotsinghchadha"
+                                icon={GitHubIcon}
+                                className="mt-4"
+                            >
+                                Follow on GitHub
+                            </SocialLink>
+                            <SocialLink
+                                href="https://linkedin.com/in/mrprabhjot/"
+                                icon={LinkedInIcon}
+                                className="mt-4"
+                            >
+                                Follow on LinkedIn
+                            </SocialLink>
+                            <SocialLink
+                                href="mailto:prabhjottechs@gmail.com"
+                                icon={MailIcon}
+                                className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
+                            >
+                                prabhjottechs@gmail.com
+                            </SocialLink>
+                        </ul>
+                    </div>
+                </div>
+            </Container>
+        </>
+    )
 }
